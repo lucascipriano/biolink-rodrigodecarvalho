d3b6fce0891e2851cfd652c9700b7ed93bd5bbd4
Lucas Cipriano <lucasncborges@gmail.com>
2024-12-24 23:34:59 -0300

    Init project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,90 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Book, Github, Instagram, Phone } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 ">
+      <Card className="w-full max-w-md mx-4 ">
+        <CardContent className="flex flex-col items-center p-6 space-y-3">
+          <Image
+            src="/rodrigo.jpg"
+            alt="Profile Picture"
+            width={128}
+            height={128}
+            className="rounded-full border-4 border-white shadow-lg"
+          />
+          <div className="flex">
+            <h1 className=" text-xl md:text-2xl font-bold text-center mr-2">
+              Pai Rodrigo de Carvalho
+            </h1>
+            <Image src="/twitter.svg" alt="" width={20} height={20} />
+          </div>
+          <p className="text-center text-gray-600">
+            O maior Espiritualista Sensitivo do Brasil - Rio de Janeiro
+          </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <div className="w-full space-y-4">
+            <Button variant="outline" className="w-full justify-start" asChild>
+              <a
+                href="https://api.whatsapp.com/send/?phone=5521991072228&text&type=phone_number&app_absent=0"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Phone className="mr-2 h-4 w-4" /> Whatsapp
+              </a>
+            </Button>
+            <Button variant="outline" className="w-full justify-start" asChild>
+              <a
+                href="https://www.instagram.com/pairodrigodecarvalho/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Instagram className="mr-2 h-4 w-4" /> Instagram
+              </a>
+            </Button>
+            <Button variant="outline" className="w-full justify-start" asChild>
+              <a
+                href="https://www.tiktok.com/@pairodrigodecarvalho?_t=8kCvb7rYsru&_r=1"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src="/tiktok-logo-4502.svg"
+                  alt=""
+                  width={19}
+                  height={19}
+                />{" "}
+                <p>Tiktok</p>
+              </a>
+            </Button>
+            <Button variant="outline" className="w-full justify-start" asChild>
+              <a
+                href="https://github.com/seuuser"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Github className="mr-2 h-4 w-4" /> GitHub
+              </a>
+            </Button>
+            <Button variant="outline" className="w-full justify-start" asChild>
+              <a
+                href="https://pay.kiwify.com.br/f3ZBDUH"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-start space-x-2"
+              >
+                <Book className="h-4 w-4 flex-shrink-0" />
+                <span className="md:whitespace-normal overflow-hidden break-words ">
+                  E-book: O Poder da Fé - Como a Religião Auxilia na Depressão e
+                  Transtornos Psicológicos
+                </span>
+              </a>
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
